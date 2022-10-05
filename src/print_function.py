7c179c6824ff8c5fddd3c37f73f76ce64d0ded45
hmtmcse <hmtm.cse@gmail.com>
2022-10-05 22:45:14 +0600

    Python print or display output

---
+++ src/print_function.py
@@ -0,0 +1,10 @@
+
+print("My name is", "Touhid")
+
+print("My name is", "Touhid", sep=" >> ")
+
+print("Python Beginner course", end=" <end> ")
+print("Now I am a Programmer")
+
+laptop = ("apple", "hp", "dell")
+print(laptop)
