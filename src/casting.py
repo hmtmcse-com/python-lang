e5cc905453092f408045c97876b670d376bb6e4b
hmtmcse <hmtm.cse@gmail.com>
2022-10-06 22:56:37 +0600

    Python constants

---
+++ src/casting.py
@@ -0,0 +1,22 @@
+# ----------------------------------------------------------------------------------------------------------------------
+# String to Integer
+integer_from_string = int("100")
+print(integer_from_string)
+
+# String to Float
+float_from_string = float("100")
+print(float_from_string)
+
+# Integer to Float
+integer_from_float = int(10.20)
+print(integer_from_float)
+
+# Integer to Float
+float_from_integer = float(10)
+print(float_from_integer)
+
+
+# Float to String
+string_from_float = str(10.30)
+print(string_from_float)
+
