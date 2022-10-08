58d191852f00d4397cb60a63a1b9fc71d0ce5b1e
hmtmcse <hmtm.cse@gmail.com>
2022-10-08 09:29:48 +0600

    Python while loop

---
+++ src/while_loop.py
@@ -0,0 +1,18 @@
+# ----------------------------------------------------------------------------------------------------------------------
+# While loop
+start_count = 1
+end_count = 6
+while start_count <= end_count:
+    print(start_count)
+    start_count += 1
+
+# ----------------------------------------------------------------------------------------------------------------------
+# While loop with else
+start_count = 1
+end_count = 6
+while start_count <= end_count:
+    print(start_count)
+    start_count += 1
+else:
+    print("Loop completed")
+
