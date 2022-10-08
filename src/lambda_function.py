4e46ca79adb386b8664176d2c45d7ec17df04927
hmtmcse <hmtm.cse@gmail.com>
2022-10-08 12:08:20 +0600

    Lambda

---
+++ src/lambda_function.py
@@ -0,0 +1,6 @@
+# Lambda example
+personal_info = lambda first_name, last_name, email: first_name + " " + last_name + " " + email
+print(personal_info("Touhid", "Mia", "hmtmcse.com@gmail.com"))
+
+sum = lambda a, b: a + b
+print(sum(5, 10))
