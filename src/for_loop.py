584ea7a87399e2dd0496e62b9166a4483c05770b
hmtmcse <hmtm.cse@gmail.com>
2022-10-08 09:06:34 +0600

    Python for loop

---
+++ src/for_loop.py
@@ -0,0 +1,22 @@
+# ----------------------------------------------------------------------------------------------------------------------
+# For loop
+color_names = ["Red", "Green", "Blue", "Yellow"]
+for name in color_names:
+    print(name)
+
+# For loop else execute when it's finish
+color_names = ["Red", "Green", "Blue", "Yellow"]
+for name in color_names:
+    print(name)
+else:
+    print("Loop Completed")
+
+print("\n ------ \n")
+
+# For loop else execute when it has no item
+color_names = []
+for name in color_names:
+    print(name)
+else:
+    print("Empty colors")
+
